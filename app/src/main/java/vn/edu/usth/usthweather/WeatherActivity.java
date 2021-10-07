aee9c2bdee35cd07e295ab2f6a75d349291141e2
Nguyen Quy Minh <incarnation1602@gmail.com>
2021-10-07 13:47:28 +0700

    Upload labwork8

--- app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
+++ app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
@@ -9,9 +9,12 @@ import androidx.viewpager2.widget.ViewPager2;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class WeatherActivity extends AppCompatActivity {
 
     private static final String TAG = "Status";
+    TabLayout tabLayout;
     ViewPager2 pager2;
     HomeFragmentPagerAdapter adapter;
 
@@ -20,12 +23,40 @@ public class WeatherActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tabLayout = findViewById(R.id.tab_layout);
         pager2 = findViewById(R.id.viewPager2);
 
         FragmentManager fm = getSupportFragmentManager();
         adapter = new HomeFragmentPagerAdapter(fm, getLifecycle());
         pager2.setAdapter(adapter);
 
+        tabLayout.addTab(tabLayout.newTab().setText("HANOI, VIET NAM"));
+        tabLayout.addTab(tabLayout.newTab().setText("PARIS, FRANCE"));
+        tabLayout.addTab(tabLayout.newTab().setText("TOULOUSE, FRANCE"));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                pager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        pager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                tabLayout.selectTab(tabLayout.getTabAt(position));
+            }
+        });
 //        FragmentManager fragment= getSupportFragmentManager();
 //        FragmentTransaction fragment_add = fragment.beginTransaction();
 //        fragment_add.add(R.id.main, new ForecastFragment());
